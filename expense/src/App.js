2136bfda19c9a0664a59506d6d0155fb8e633b07
Himanshu Chandola <96554303+himanshuchandola@users.noreply.github.com>
2022-05-26 19:01:18 +0530

    Added app.js

--- expense/src/App.js
+++ expense/src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import "./App.css";
+import { Provider } from "react-redux";
+import store from "./reduxstore/store";
+import Header from "./components/common/header";
+import AllExpenses from "./components/all-expenses";
+import AddExpense from "./components/add-expense";
+import ExpenseAnalysis from "./components/expense-analysis";
+import DueExpenses from "./components/due-expenses";
+import Footer from "./components/common/footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="app">
+        <Router>
+          <Header />
+          <Switch>
+            <Route exact path="/">
+              <AllExpenses />
+            </Route>
+            <Route exact path="/add-expense">
+              <AddExpense />
+            </Route>
+            <Route exact path="/expenses-analysis">
+              <ExpenseAnalysis />
+            </Route>
+            <Route exact path="/due-expenses">
+              <DueExpenses />
+            </Route>
+          </Switch>
+          <Footer />
+        </Router>
+      </div>
+    </Provider>
   );
 }
 
